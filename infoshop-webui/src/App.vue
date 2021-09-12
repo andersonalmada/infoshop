97eefe7062d06f436b24ad078520db604dd1a2e1
Anderson Almada <andersonalmadagomes@gmail.com>
2021-09-12 18:05:48 -0300

    add webui

---
+++ infoshop-webui/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Product </router-link>
+    <router-link to="/feedback"> | Feedback </router-link>
+    <router-link to="/ranking"> | Ranking </router-link>
+  </div>
+  <router-view />
+</template>
---
+++ infoshop-webui/src/router/index.js
@@ -0,0 +1,36 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Product from "@/views/Product.vue";
+import Feedback from "@/views/Feedback.vue";
+import Ranking from "@/views/Ranking.vue";
+import NotFound from "@/views/NotFound.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Product",
+    component: Product,
+  },
+  {
+    path: "/product",
+    name: "Product2",
+    component: Product,
+  },
+  {
+    path: "/feedback",
+    name: "Feedback",
+    component: Feedback,
+  },
+  {
+    path: "/ranking",
+    name: "Ranking",
+    component: Ranking,
+  },
+  { path: "/:pathMatch(.*)*", name: "not-found", component: NotFound },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;

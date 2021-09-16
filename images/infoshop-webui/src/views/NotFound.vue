f4305b8e8358cd6fc6fca70584d86094267299e9
Anderson Almada <andersonalmadagomes@gmail.com>
2021-09-16 09:13:10 -0300

    remove all folders

--- images/infoshop-webui/src/views/NotFound.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  Not Found
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
--- infoshop-webui/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home </router-link>
-    <router-link to="/ranking"> | Ranking </router-link>
-  </div>
-  <router-view />
-</template>
